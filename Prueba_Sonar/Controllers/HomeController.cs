316877dc5ed11ae6709d3b97454f2e04bef07fa4
Daniel Mora <ingdanieljm@bythewave.co>
2023-04-20 08:53:06 -0500

    Fix bug sonar 0.0.1

--- Prueba_Sonar/Controllers/HomeController.cs
+++ Prueba_Sonar/Controllers/HomeController.cs
@@ -6,11 +6,9 @@ namespace Prueba_Sonar.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
